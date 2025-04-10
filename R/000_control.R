8555a736802865a4470934dcc3fd8d6cbc93b647
dougtommet <doug_tommet@brown.edu>
2025-04-10 15:16:34 -0400

    Updated the crosswalk figure

--- R/000_control.R
+++ R/000_control.R
@@ -1,8 +1,8 @@
 
-quarto::quarto_render(here::here("R", "000-master.qmd"), output_format = "html")
+quarto::quarto_render(here::here("R", "000_master.qmd"), output_format = "html")
 
 
-fs::file_move(here::here("R", "000-master.html"),
+fs::file_move(here::here("R", "000_master.html"),
               here::here("Reports", stringr::str_c("CCI-GCP-equating_", Sys.Date(),".html")))
 
 
